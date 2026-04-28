27dff76436720e49d7faccf64378f261e16b4914
azathd <azathd@yandex.ru>
2026-04-28 12:59:29 +0300

    init

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,12 +1,14 @@
 const API_BASE_URL = 'http://localhost:8000/api'
 
 export async function apiRequest(path, options = {}) {
+  const { headers: customHeaders = {}, ...requestOptions } = options
+
   const response = await fetch(`${API_BASE_URL}${path}`, {
+    ...requestOptions,
     headers: {
       'Content-Type': 'application/json',
-      ...(options.headers || {}),
+      ...customHeaders,
     },
-    ...options,
   })
 
   const isJson = response.headers.get('content-type')?.includes('application/json')
